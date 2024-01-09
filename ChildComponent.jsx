4d279d2ddb172b86cb215fc7722dc73e260788e9
ngberces <98504159+ngberces@users.noreply.github.com>
2024-01-09 14:44:13 +0800

    Add files via upload

---
+++ ChildComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+//function ChildComponent(props) {
+//    return(
+//        <div>
+ //           <p>{props.message}</p>
+//        </div>
+//    );
+
+//}
+class ChildComponent extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>{this.props.message}</p>
+            </div>
+        )
+    }
+}
+
+export default  ChildComponent;
